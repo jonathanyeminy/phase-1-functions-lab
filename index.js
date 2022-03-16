b713b1d02810aaf050a59ad7ec555bb6d6c0f316
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-03-15 20:51:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(destStreet){
+    return Math.abs(42-destStreet)
+}
+
+function distanceFromHqInFeet(destStreet) {
+    return distanceFromHqInBlocks(destStreet) * 264
+}
+
+function distanceTravelledInFeet(start,destination){
+    return (Math.abs(destination-start)*264)
+}
+
+function calculatesFarePrice(start,destination){
+    if (distanceTravelledInFeet(start,destination) > 2500) {
+        return 'cannot travel that far'
+    }
+    else if (distanceTravelledInFeet(start,destination) > 2000){
+        return 25
+    }
+    else if (distanceTravelledInFeet(start,destination) > 400) {
+        return (distanceTravelledInFeet(start,destination)-400) * .02
+    }
+    else if (distanceTravelledInFeet(start,destination) <= 400) {
+        return 0
+    }
+}
